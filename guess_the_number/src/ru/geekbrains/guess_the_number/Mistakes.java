09c898b2cecb9487db6189a9beeba0b5d7f1881d
gintariukai <gintariukai@hotmail.com>
2020-06-29 22:24:15 +0300

    Add new file
    add file mistakes

--- guess_the_number/src/ru/geekbrains/guess_the_number/Mistakes.java
+++ guess_the_number/src/ru/geekbrains/guess_the_number/Mistakes.java
@@ -2,8 +2,25 @@ package ru.geekbrains.guess_the_number;
 
 public class Mistakes {
 
-    public static void main() {
-        char c = "Hello";
-        System.out.println(c);
+    public static void main2(String[] args) {
+//        char c1 = "Hello!";  ошибка
+        String c1 = "Hello!";
+        System.out.println(c1);
+
     }
+
+    public static void main4(String[] args) {
+//        String q = 'Hello';
+        String q = "Hello";
+        System.out.println(q);
+    }
+
+    public static void main6(String[] args){
+        int a = 20;
+        String b = "myNumbeer = ";
+        a *=a;
+        System.out.println(b + a);
+    }
+
+
 }
